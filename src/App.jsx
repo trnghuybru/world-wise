2f9c1e3902c6002dc9508fcc6eaecb5f2a7bcf47
trnghuybru <huytr222@gmail.com>
2024-08-13 22:48:52 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNoteFound from "./pages/PageNoteFound";
+import Login from "./pages/Login";
+import AppLayout from "./pages/AppLayout";
+import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
+import CountryList from "./components/CountryList";
+
+const BASE_URL = "http://localhost:8000";
+
+function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There was an error loading data");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
+        <Route path="*" element={<PageNoteFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/PageNoteFound.jsx
@@ -0,0 +1,11 @@
+import PageNav from "../components/PageNav";
+
+function PageNoteFound() {
+  return (
+    <div>
+      <PageNav /> Not Founds
+    </div>
+  );
+}
+
+export default PageNoteFound;
